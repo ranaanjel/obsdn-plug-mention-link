129d61c65e86cceb395c1b28282484301d5ec95b
ranaanjel <anjelranawork@gmail.com>
2024-07-30 18:59:20 +0530

    first version of the automention plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,145 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { parse as p } from "node-html-parser";
+import {exec} from "child_process"
+import {promisify} from "util"
+import { Url } from 'url';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const execP = promisify(exec)
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	 imageURL :string ;
+	async onload():Promise<void> {
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
+		//console.log("this is first time from plugins")
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		//const statusBarItemEl = this.addStatusBarItem();
+		//statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.app.workspace.on("editor-paste", async (eObj, ed, info) => {
+
+			eObj.preventDefault();
+
+			let cpData = eObj.clipboardData ;
+			
+			let value:string = cpData?.getData("text");
+			let pattern:RegExp = /(https|http)/;
+			let n = Number(value?.length); 
+			const cursorPosition = ed.getCursor()
+			let cursorPosition2 = Object.assign({}, cursorPosition)
+			cursorPosition2.ch = n;
+
+			//console.log(pattern.test(value))
+
+			if(!pattern.test(value)) {
+				ed.replaceRange(value, cursorPosition)
+				ed.setCursor(cursorPosition2)
+				return "";
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			//console.log(ed.getCursor())
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+			let boilterValue = '[Auto Mentioning in a sec...]()' 
+			let boilerLength = boilterValue.length
+			
+			cursorPosition2.ch = boilerLength; 
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			ed.replaceRange(boilterValue, cursorPosition)
+			
+			//console.log(cursorPosition, cursorPosition2)	
+			//ed.setSelection( cursorPosition, cursorPosition2)
+		
+			let tValue = 	await this.getTitle(value)
+			//console.log(tValue)
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+			let replace = `[${tValue}](${value})`
+			//cursorPosition2.ch = replace.length;
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			//console.log(cursorPosition2)
+			//ed.setCursor(cursorPosition2)
+
+			//ed.replaceSelection(replace)
+			//ed.replaceRange(replace, cursorPosition)
+			//ed.setCursor(cursorPosition2)
+
+
+			console.log(ed.getLine(cursorPosition2.line))
+			let imgElement = document.createElement("img")
+			imgElement.src = this.imageURL
+			imgElement.setAttribute("width", "15")
+			imgElement.setAttribute("height", "15")
+			console.log(imgElement.outerHTML)
+			
+			let insertTitle = imgElement.outerHTML+" "+replace;
+
+			ed.setLine(cursorPosition2.line, insertTitle)
+			cursorPosition2.ch = insertTitle.length;
+			ed.setCursor(cursorPosition2)
+
+			//let setValue = ed.setSelection(cursorPosition2, cursorPosition);
+			//ed.replaceRange("hello world", cursorPosition)			
+			
+			//console.log(cursorPosition,cursorPosition2,value)
+			//https://publish-01.obsidian.md/access/caa27d6312fe5c26ebc657cc609543be/favicon-96x96.png
+		})
+		
+	}
+	private async getTitle(url:string):Promise<string>{
+		
+	 //let pattern:RegExp = /(?:\.?)(\w+)(?:\.)/g;
+	//	console.log(url.match(pattern),url)
+	
+   //let domainName = url.match(pattern)[1];
+	
+	//  if(!domainName) {
+	// 	console.log('something with url')
+	// 	return ;
+	//  }
+
+	let htmlText = "";
+	let command = "curl '" + url + "'"; 
+  let {stdout} = await execP(command)
+
+	//console.log(stdout)
+	htmlText = stdout;
+	const doc = p(htmlText);
+  
+	
+  let titleValue:string = doc.querySelector("title")?.text || "" ;
+	let valueURL = new URL(url)
+
+	if(titleValue.includes("Your browser is deprecated, please upgrade.")) {
+		titleValue = valueURL.hostname;
 	}
 
-	onunload() {
+	// console.log(titleValue, "something new")
+	let iconURL = doc.querySelector("[rel='icon']")?.attributes.href || "novalue"; // icon value without downloading.
 
+	if(iconURL[0] == "/") {
+		let originValue = valueURL.origin ;
+		//console.log(valueURL)
+		iconURL = valueURL+iconURL;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	this.imageURL = iconURL;
+
+	console.log(iconURL, doc, titleValue)	
+
+	return titleValue;
+	
+	// try {
+	//cannot use the fetch as the origin is custom and blocked by website's server CORS policy
+  //   //const response = await fetch(url);
+  //   //if (!response.ok) {
+  //   //  throw new Error(`Response status: ${response.status}`);
+  //   //}
+
+   
+   
+
+  // } catch (e) {
+  //   console.error(e);
+  // }
 	}
+	
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+
 	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
